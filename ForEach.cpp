f239906816a3190a44c31942b2f77da093270ec4
Dark Mortal <chodab74@gmail.com>
2021-08-28 22:11:01 +0530

    Added a new File

---
+++ ForEach.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int main(){
+   vector<int> arr={1,2,3,4,5,6,7,8,9,10};
+   stable_sort(arr.begin(),arr.end());
+   reverse(arr.begin(),arr.end());
+   for(int i:arr) cout<<(i*i+2*i+1)<<" "; cout<<endl<<endl;
+   vector<string> Names={"Saptarshi Dey","Dipanjan Biswas","Swagatam Burai","Rishav Gupta"};
+   stable_sort(Names.begin(),Names.end());
+   for(string name: Names) cout<<name<<endl;
+   return 0;
+}
