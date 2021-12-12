291f341ea8b3ba9895ac7a136bac79d3188a7c68
Dark Mortal <chodab74@gmail.com>
2021-12-12 09:18:43 +0530

    Polynomial Equations

--- Roots.cpp
+++ Roots.cpp
@@ -1,20 +1,10 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-const std::unordered_map<int, std::string> kSuperscripts = {
-        { 0, "⁰" },
-        { 1, "¹" },
-        { 2, "²" },
-        { 3, "³" },
-        { 4, "⁴" },
-        { 5, "⁵" },
-        { 6, "⁶" },
-        { 7, "⁷" },
-        { 8, "⁸" },
-        { 9, "⁹" },
-};
+//Only works in Linux
+static char *super[] = {"\xe2\x81\xb0", "\xc2\xb9", "\xc2\xb2", "\xc2\xb3", "\xe2\x81\xb4", "\xe2\x81\xb5", "\xe2\x81\xb6","\xe2\x81\xb7", "\xe2\x81\xb8", "\xe2\x81\xb9"};
 
-void combinationUtil(int arr[], int data[],int start, int end,int index, int r,int &elm){
+void combinationUtil(float arr[], float data[],int start, int end,int index, int r,float &elm){
 	if (index == r){
         int x=1;
 		for (int j = 0; j < r; j++) x*=data[j];
@@ -27,8 +17,8 @@ void combinationUtil(int arr[], int data[],int start, int end,int index, int r,i
 	}
 }
 
-void sumCombination(int arr[], int n, int r,int &x){
-	int data[r];
+void sumCombination(float arr[], int n, int r,float &x){
+	float data[r];
 	combinationUtil(arr, data, 0, n-1, 0, r,x);
 }
 
@@ -38,13 +28,13 @@ int main()
 	cout<<"Enter number of Roots = "; cin>>n;
 	if(n>=10) cout<<"Equations of degree more than 9 are not supported"<<endl;
 	else{
-        int arr[n];
+        float arr[n];
         cout<<"Enter the Roots : ";
         for(int j=0;j<n;j++) cin>>arr[j];
-        cout<<"Enter variable : "; getchar(); cin>>ch; cout<<ch<<kSuperscripts.at(n);
+        cout<<"Enter variable : "; getchar(); cin>>ch; cout<<ch<<super[n];
         for(int i=1;i<=n;i++){
-            int r=i,sum=0;
-            sumCombination(arr, n, r,sum);
+            float sum=0.0;
+            sumCombination(arr, n, i,sum);
             if(sum){
                 sum=(i%2==0)?(sum):(sum*(-1));
                 if(sum>0 && sum!=1) cout<<"+"<<sum;
@@ -53,7 +43,7 @@ int main()
                 else cout<<sum;
                 if(n!=i){
                     if((n-i)==1) cout<<ch;
-                    else cout<<ch<<kSuperscripts.at(n-i);
+                    else cout<<ch<<super[n-i];
                 }
             }
     }}
