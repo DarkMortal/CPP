9e0f990bcf35a6c55fe3b6dfc12cd84c06e50857
Dark Mortal <chodab74@gmail.com>
2021-12-13 21:04:43 +0530

    Polynomial Equations

--- Roots.cpp
+++ Roots.cpp
@@ -4,6 +4,20 @@ using namespace std;
 //Only works in Linux
 static char *super[] = {"\xe2\x81\xb0", "\xc2\xb9", "\xc2\xb2", "\xc2\xb3", "\xe2\x81\xb4", "\xe2\x81\xb5", "\xe2\x81\xb6","\xe2\x81\xb7", "\xe2\x81\xb8", "\xe2\x81\xb9"};
 
+void Disp(int n){
+    int s=0,zeros=0,x=n;
+    while(x){
+        s=10*s+x%10;
+        if(x%10==0) zeros++;
+        x/=10;
+    }
+    while(s){
+        cout<<super[s%10];
+        s/=10;
+    }
+    for(int i=0;i<zeros;i++) cout<<super[0];
+}
+
 void combinationUtil(float arr[], float data[],int start, int end,int index, int r,float &elm){
 	if (index == r){
         float x=1.0;
@@ -26,26 +40,23 @@ int main()
 {
 	int n; char ch;
 	cout<<"Enter number of Roots = "; cin>>n;
-	if(n>=10) cout<<"Equations of degree more than 9 are not supported"<<endl;
-	else{
-        float arr[n]={0};
-        cout<<"Enter the Roots : ";
-        for(int j=0;j<n;j++) cin>>arr[j];
-        cout<<"Enter variable : "; getchar(); cin>>ch; cout<<ch<<super[n];
-        for(int i=1;i<=n;i++){
-            float sum=0.0;
-            sumCombination(arr, n, i,sum);
-            if(sum){
-                sum=(i%2==0)?(sum):(sum*(-1));
-                if(sum>0 && sum!=1) cout<<"+"<<sum;
-                else if(sum==-1) cout<<"-";
-                else if(sum==1) cout<<"+";
-                else cout<<sum;
-                if(n!=i){
-                    if((n-i)==1) cout<<ch;
-                    else cout<<ch<<super[n-i];
-                }
+    float arr[n]={0};
+    cout<<"Enter the Roots : ";
+    for(int j=0;j<n;j++) cin>>arr[j];
+    cout<<"Enter variable : "; getchar(); cin>>ch; cout<<ch; Disp(n);
+    for(int i=1;i<=n;i++){
+        float sum=0.0;
+        sumCombination(arr, n, i,sum);
+        if(sum){
+            sum=(i%2==0)?(sum):(sum*(-1));
+            if(sum>0 && sum!=1) cout<<"+"<<sum;
+            else if(sum==-1) cout<<"-";
+            else if(sum==1) cout<<"+";
+            else cout<<sum;
+            if(n!=i){
+                if((n-i)==1) cout<<ch;
+                else{ cout<<ch; Disp(n-i); }
             }
-    }}
-	cout<<endl; return 0;
+        }
+    } cout<<endl; return 0;
 }
