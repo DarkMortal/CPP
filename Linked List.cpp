3b96da5ebe8761e22bf0b8f80e9a454437ff54e7
Dark Mortal <67017303+DarkMortal@users.noreply.github.com>
2022-03-02 11:57:12 +0530

    Update Linked List.cpp

--- Linked List.cpp
+++ Linked List.cpp
@@ -14,22 +14,18 @@ struct Node{
 int main(){
     int n=0;   //Number of Elements of the List
     cout<<"Enter the number of Names : "; cin>>n;
-    if(n>2){
+    if(n<4) cout<<"Number of Names must be greater than 3"<<endl;
+    else {
         string input="";
         cout<<"Enter the Names : "<<endl; getchar();
 
         //Initialising the List
         getline(cin,input);
         start.name=input;
-        getline(cin,input);
-
-        //Initialising the First and second element of the List
-        start.next=new Node(input,2);
-        start.next->previous=&start;
-        Node* current=start.next;
+        Node* current=&start;
 
         //Initialising rest of the Elements
-        for(int i=2;i<n;i++){ 
+        for(int i=1;i<n;i++){ 
             getline(cin,input);
             current->next=new Node(input,i+1);
             current->next->previous=current;
@@ -55,16 +51,16 @@ int main(){
         cout<<"\nAfter deleting the third Element"<<endl;
         //Displaying the values and deallocating memory
         do{
-            if(current){
+            if(current==&start){
+                current->Display(); 
+                delete current; break;
+            }
+            else if(current){
                 temp=current->previous;
                 current->Display();
                 delete current;
                 current=temp;
             }
-            else if(current==&start){
-                current->Display(); break;
-            }
         }while(current->previous || current==&start);
-    }else cout<<"Number of Names must be greater than 2"<<endl;
-    return 0;
+    } return 0;
 }
