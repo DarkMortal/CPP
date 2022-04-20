754d8a62c60fba852f304385bcf7bdfbc15cdea2
Dark Mortal <67017303+DarkMortal@users.noreply.github.com>
2022-04-20 19:16:56 +0530

    Add files via upload

---
+++ Exponents.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+double exp(double x){
+    double a=x,sum=0;
+    const unsigned int n=20;
+    for(float i=1.0;i<=n;i+=1.0){
+        sum+=a;
+        a*=(x/(i+1));
+    }
+    return sum+1;
+}
+
+double LambertW(double x,int iterations=100){
+    if(iterations==1) return log(x);
+    else return log(x)-log(LambertW(x,iterations-1));
+}
+
+int main(){
+	double x=LambertW(5);
+	cout<<x*exp(x)<<endl<<exp(1.0)<<endl;
+}
