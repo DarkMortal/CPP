c342513a682f697862d23e967101d632844d6eae
Dark Mortal <chodab74@gmail.com>
2023-05-05 23:44:21 +0530

    Updated some code

--- DS Implementations/LambertW.cpp
+++ DS Implementations/LambertW.cpp
@@ -1,14 +1,27 @@
-#include<iostream>
-#include<math.h>
-using namespace std;
+#include<bits/stdc++.h>
 
-double LambertW(double x,int iterations=100){
-    if(iterations==1) return log(x);
+class ValueOutOfBounds : public std::exception {
+    private: char* message;
+    public: ValueOutOfBounds(char* err):message(err){}
+    char* what () { return message; }
+};
+
+double LambertW(double x,uint16_t iterations = 100){
+    if(x<0) throw ValueOutOfBounds("Value must be greater than or equal to 0");
+    if(iterations == 1) return log(x);
     else return log(x)-log(LambertW(x,iterations-1));
 }
 
+// Driver Code
 int main(){
-    double a; cin>>a;
-    a=LambertW(a);
-    cout<<a<<endl;
+    try{
+        double x,y; 
+        std::cout<<"Enter number (greater than equal to 0) : ";
+        std::cin>>x; 
+        y = LambertW(x);
+        std::cout<<"W("<<x<<") = "<<std::setprecision(6)<<y<<std::endl;
+        std::cout<<y<<"*exp("<<y<<") = "<<y*exp(y)<<std::endl;
+    }catch(ValueOutOfBounds err){
+        std::cout<<err.what()<<std::endl;
+    }
 }
